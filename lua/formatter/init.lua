b6ddd388678953995bc69adfdda40fdb233c96de
houzheng <capiiii@outlook.com>
2023-03-29 11:16:39 +0800

    update-null-ls-config

--- lua/formatter/init.lua
+++ lua/formatter/init.lua
@@ -1,6 +1,23 @@
 local formatters = require "lvim.lsp.null-ls.formatters"
+
+local null_ls_h = require("null-ls.helpers")
+
 formatters.setup {
-  { command = "prettierd",
+  null_ls.builtins.formatting.prettier.with({
+    command = "npx",
+    args = null_ls_h.range_formatting_args_factory({
+      "prettier",
+      "--stdin-filepath",
+      "$FILENAME",
+    }, "--range-start", "--range-end", { row_offset = -3, col_offset = -1 }),
+  }),
+  -- { command = "prettierd",
+  --   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html",
+  --     "json", "jsonc", "yaml", "markdown", "markdown.mdx", "graphql", "handlebars" } }
+  {
+    commang = "eslint_d",
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html",
-      "json", "jsonc", "yaml", "markdown", "markdown.mdx", "graphql", "handlebars" } }
+      "json", "jsonc", "yaml", "markdown" },
+    args = { "--fix" }
+  }
 }
--- lua/linter/init.lua
+++ lua/linter/init.lua
@@ -2,3 +2,17 @@ local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
   { command = "eslint_d", filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" } },
 }
+
+local code_actions = require "lvim.lsp.null-ls.code_actions"
+code_actions.setup {
+  {
+    command = "eslint",
+    args = { "-f" },
+    filetypes = {
+      "javascriptreact",
+      "javascript",
+      "typescriptreact",
+      "typescript",
+    },
+  },
+}
